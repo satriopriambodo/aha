0e7ffae917f2060124b21e1886d1160047c5172e
satrio priambodo <satriopriambodo96@gmail.com>
2022-10-15 11:34:31 +0700

    push heroku

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const route = require("./routes/index");
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(route);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+module.exports = app;
